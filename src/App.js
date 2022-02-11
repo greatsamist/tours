e6c9d27c0bce18db4ef9494e8bb9efa4c299c6d9
Samuel Ojo <ifegbenga@gmail.com>
2022-02-11 23:57:03 +0100

    state added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { useState } from "react";
 import Styles from "./App.module.scss";
 import Tour from "./components/Tour";
 // import Card from "./components/Card.module.scss";
-
+const url = "https://course-api.com/react-tours-project";
 function App() {
+  const [loading, setIsLoading] = useState(true);
+  const [tour, setTours] = useState();
+
+  if (loading) {
+    return (
+      <main className={Styles.loading}>
+        <p>Loading...</p>
+      </main>
+    );
+  }
   return (
     <div className={Styles.container}>
       <h1 className={Styles["container__h1"]}>Our Tours</h1>
